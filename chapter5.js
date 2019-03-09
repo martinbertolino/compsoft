ad3613b6d43e5f55dd4aaa50d16535d35be0a53e
Martin Bertolino <martinbertolino@gmail.com>
2019-03-08 20:46:24 -0600

    Minor changes and added chapter 5

---
+++ chapter5.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const addToCart = (cart, item, quantity) => {
+	return {
+		...cart,
+		items: cart.items.concat([{
+			item,
+			quantity
+		}]),
+	};
+}
+
+const originalCart = {
+	items: []
+};
+
+const newCart = addToCart(
+		originalCart,
+		{
+			name: 'Digital SLR Camera',
+			price: '1495'
+		},
+		1
+);
+
+console.log(JSON.stringify(originalCart, null, 2));
+
+console.log(JSON.stringify(newCart, null, 2));
+
+
+
